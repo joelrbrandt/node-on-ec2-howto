43062af30fe374a060b161fb01b6fd489cbb9f8b
Joel Brandt <joelrbrandt@gmail.com>
2013-03-05 21:54:38 +0000

    more work

---
+++ example/server.js
@@ -0,0 +1,27 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true,
+indent: 4, maxerr: 50, node: true */
+
+(function () {
+    "use strict";
+    
+    var https = require("https");
+    var fs = require("fs");
+    var connect = require("connect");
+    
+    var options = {
+        key: fs.readFileSync("cert/certificate.key"),
+        cert: fs.readFileSync("cert/certificate.cert")
+    };
+    
+    var app = connect()
+        .use(connect.logger("dev"))
+        .use(connect["static"]("public"))
+        .use(function (req, res) {
+            res.end("hello world\n");
+        });
+    
+    var server = https.createServer(options, app)
+        .listen(3443, "0.0.0.0");
+    
+    console.log("Server running at http://0.0.0.0:3443/");
+}());
